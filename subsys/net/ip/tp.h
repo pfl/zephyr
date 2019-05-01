380ad96556effb5ebbf3a022e44f9eb088958e31
Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
2019-05-01 18:33:05 +0300

    net: tp: Add test protocol
    This is JSON based test protocol (UDP) implementation.
    
    Signed-off-by: Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>

---
+++ subsys/net/ip/tp.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018-2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef TP_H
+#define TP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TP_H */
