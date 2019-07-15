96860625f9e6892fec148ab30f846c6903e4f8cf
Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>
2019-07-15 14:14:52 +0300

    net: tp: Add a header for the test protocol private declarations
    Add a header for the test protocol private declarations.
    
    Signed-off-by: Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>

--- subsys/net/ip/tp.c
+++ subsys/net/ip/tp.c
@@ -5,3 +5,4 @@
  */
 
 #include "tp.h"
+#include "tp_priv.h"
---
+++ subsys/net/ip/tp_priv.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018-2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef TP_PRIV_H
+#define TP_PRIV_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TP_PRIV_H */
