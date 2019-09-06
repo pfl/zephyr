pfl/zephyr

commit 0261ecdee480f3d94fed3a82f053d0df2e30c8cf

net: ip: net_pkt: Add pkt_cursor_advance before contiguous check
When net_pkt_skip skips the entire data in a net buffer, the cursor
still points to this buffer on data that is off by one.
Calling pkt_cursor_advance in net_pkt_is_contiguous fixes this and
moves the cursor to the next buffer.

Signed-off-by: Alexander Wachter <alexander.wachter@student.tugraz.at>

subsys/net/ip/net_pkt.c
@@ -1827,6 +1827,8 @@ u16_t net_pkt_get_current_offset(struct net_pkt *pkt)
 
 bool net_pkt_is_contiguous(struct net_pkt *pkt, size_t size)
 {
+	pkt_cursor_advance(pkt, !net_pkt_is_being_overwritten(pkt));
+
 	if (pkt->cursor.buf && pkt->cursor.pos) {
 		size_t len;
 

commit cb68e33eb00342f912320cbdca6f15dce8cac386

net: tcp2: Temp commit
This is a temp commit to workaround the regression
caused by 0261ecdee480f3d94fed3a82f053d0df2e30c8cf.

Signed-off-by: Oleg Zhurakivskyy <oleg.zhurakivskyy@intel.com>

subsys/net/ip/net_pkt.c
@@ -1883,8 +1883,6 @@ u16_t net_pkt_get_current_offset(struct net_pkt *pkt)
 
 bool net_pkt_is_contiguous(struct net_pkt *pkt, size_t size)
 {
-	pkt_cursor_advance(pkt, !net_pkt_is_being_overwritten(pkt));
-
 	if (pkt->cursor.buf && pkt->cursor.pos) {
 		size_t len;
 